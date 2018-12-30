5aeaa6001c2de0ba072750c0f3d2be3beeee59a1
DevikaLak <46183395+DevikaLak@users.noreply.github.com>
2018-12-29 19:40:16 -0500

    Implemented review comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -33,11 +33,15 @@ $(function() {
          it('all feeds\' valid url defined', function() {
            //loop to iterate over each feed
            for (let feed of allFeeds) {
-             //test to ensure URL defined
-             expect(feed.url).toBeDefined();
+             /*Replacing following test with Reviewer suggestion
+             1) test to ensure URL defined
+             2) test to ensure URL is not empty
+             */
+             //expect(feed.url).toBeDefined();
+             //expect(feed.url.length).not.toBe(0);
 
-             //test to ensure URL is not empty
-             expect(feed.url.length).not.toBe(0);
+             //Added reviewer suggestion
+             expect(feed.url).toBeTruthy();
            }
          });
 
@@ -48,11 +52,15 @@ $(function() {
          it('all feeds\' valid name defined', function() {
            //loop to iterate over each feed
            for (let feed of allFeeds) {
-             //test to ensure name defined
-             expect(feed.name).toBeDefined();
+             /*Replacing following test with Reviewer suggestion
+             1) test to ensure name defined
+             2) test to ensure name is not empty
+             */
+             //expect(feed.name).toBeDefined();
+             //expect(feed.name.length).not.toBe(0);
 
-             //test to ensure name is not empty
-             expect(feed.name.length).not.toBe(0);
+             //Added reviewer suggestion
+             expect(feed.name).toBeTruthy();
            }
          });
     });
@@ -69,8 +77,8 @@ $(function() {
            let bodyElem = document.querySelector('body');
            expect(bodyElem).toBeDefined();
 
-           expect(bodyElem.className).toEqual('menu-hidden');
-           expect(bodyElem.className.length).not.toBe(0);
+           expect(bodyElem.classList.contains('menu-hidden')).toBe(true);
+           expect(bodyElem.classList.length).not.toBe(0);
          });
 
          /* A test that ensures the menu changes
@@ -87,10 +95,10 @@ $(function() {
 
             //on first click
             menuLinkElem.click();
-            expect(bodyElem.className.length).toBe(0);
+            expect(bodyElem.classList.contains('menu-hidden')).not.toBe(true);
             //on second click
             menuLinkElem.click();
-            expect(bodyElem.className).toEqual('menu-hidden');
+            expect(bodyElem.classList.contains('menu-hidden')).toBe(true);
           });
     });
 
@@ -118,7 +126,7 @@ $(function() {
                loadFeed(i, function() {
                  feedContainer = document.querySelector('.feed');
                  if (feedContainer !== undefined) {
-                   allFeedAnchors = feedContainer.querySelectorAll('a');
+                   allFeedAnchors = feedContainer.querySelectorAll('.feed .entry');
                  }
                  done();
                });
@@ -142,43 +150,78 @@ $(function() {
          */
 
          /* Logic:
-          * Setting previous top link to empty. Then invoking load feed for
-          * first link and ensuring that the top link value for this link
-          * isn't the same as previous link. Then, setting this value and
-          * previous and continuing the test for other links in similar
-          * fashion
+          * Before executing test spec for each feed link, setting the top feed
+          * from previous link and current link in 'beforeEach'. Jasmine 'done' is
+          * used to ensure that the asynchronous invocation to loadFeed completes
+          * before executing the Test spec in 'it' function
           */
-         let feedContainer, allFeedAnchors, previousTopFeedLink, currTopFeedLink;
-         beforeAll(function() {
-           console.log("Perfect???");
-           //initializing previous top feed to empty value for once
-           previousTopFeedLink = '';
-         });
-         //iterating over each fee link
-         for (let j = 0; j < allFeeds.length; j++) {
-           it(`content for ${allFeeds[j].name} has changed`, function(done) {
-             //executing loadFeed asynchronously
-             loadFeed(j, function() {
-               /* In callback function of load feed fetching the first anchor
-                * link from feed container of document loaded
-               */
-               feedContainer = document.querySelector('.feed');
-               if (feedContainer !== undefined) {
-                 allFeedAnchors = feedContainer.querySelectorAll('a');
-                 if (allFeedAnchors !== undefined && allFeedAnchors.length > 0) {
-                   currTopFeedLink = allFeedAnchors[0].href;
-                 }
-               }
-               // ensuring this link is not same as previously saved top feed
-               expect(currTopFeedLink).toBeDefined();
-               expect(currTopFeedLink).not.toEqual(previousTopFeedLink);
-               /* updating the previously saved top feed with current value
-                * for future iterations
-                */
-               previousTopFeedLink = currTopFeedLink;
-               done();
+           let feedContainer, allFeedAnchors, previousTopFeedLink, currTopFeedLink, j;
+
+           beforeAll(function(){
+             j = 0;
+           });
+           beforeEach(function(done){
+              loadFeed(j, function(){
+                  /* In callback function of load feed fetching the first feed
+                   * from feed container of document loaded
+                  */
+                  feedContainer = document.querySelector('.feed');
+                  if (feedContainer !== undefined) {
+                    allFeedAnchors = feedContainer.querySelectorAll('a');
+                    if (allFeedAnchors !== undefined && allFeedAnchors.length > 0) {
+                      //Setting value of top feed for previous feed link
+                      previousTopFeedLink = allFeedAnchors[0].href;
+                    }
+                  }
+                  let k;
+                  if (j === 3) {
+                    k = 0;
+                  } else {
+                    k = j+1
+                  }
+                  loadFeed( k, function(){
+                    /* In callback function of load feed fetching the first feed
+                     * from feed container of document loaded
+                    */
+                    feedContainer = document.querySelector('.feed');
+                    if (feedContainer !== undefined) {
+                      allFeedAnchors = feedContainer.querySelectorAll('a');
+                      if (allFeedAnchors !== undefined && allFeedAnchors.length > 0) {
+                        //Setting value of top feed for current feed link
+                        currTopFeedLink = allFeedAnchors[0].href;
+                      }
+                    }
+                    done();
+                 });
              });
            });
-         }
+
+           afterEach(function(){
+             j++;
+           });
+
+           it(`content for ${allFeeds[0].name} has changed`, function() {
+                 // ensuring this feed is not same as previously saved top feed
+                 expect(currTopFeedLink).toBeTruthy();
+                 expect(currTopFeedLink).not.toEqual(previousTopFeedLink);
+           });
+
+           it(`content for ${allFeeds[1].name} has changed`, function() {
+                 // ensuring this feed is not same as previously saved top feed
+                 expect(currTopFeedLink).toBeTruthy();
+                 expect(currTopFeedLink).not.toEqual(previousTopFeedLink);
+           });
+
+           it(`content for ${allFeeds[2].name} has changed`, function() {
+                 // ensuring this feed is not same as previously saved top feed
+                 expect(currTopFeedLink).toBeTruthy();
+                 expect(currTopFeedLink).not.toEqual(previousTopFeedLink);
+           });
+
+           it(`content for ${allFeeds[3].name} has changed`, function() {
+                 // ensuring this feed is not same as previously saved top feed
+                 expect(currTopFeedLink).toBeTruthy();
+                 expect(currTopFeedLink).not.toEqual(previousTopFeedLink);
+           });
     });
 }());
